1f350df3e911ef429760e7f65ba783dd64a197d8
yoh-space <yohannesdamtie822@gmail.com>
2026-06-24 12:21:42 +0300

    first js code

---
+++ script.js
@@ -0,0 +1,13 @@
+let appName = "My App";
+const showAppName = () => {
+  let message = "welcome to My App";
+  console.log(message);
+};
+
+// showAppName();
+// console.log(appName);
+
+age = 18;
+age = 'Name';
+
+console.log(age);
